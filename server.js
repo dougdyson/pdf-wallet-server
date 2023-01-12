75969a05e6b90b634fd674d7acbb2eef2c16ec91
Unfamous Dyson <dougdyson@gmail.com>
2023-01-11 23:26:50 -0800

    update comment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(fileUpload());
 app.use(cors());
 
-// need to include headers to allow CORS and authentication.
+// need to include protection against CSRF
 
 app.post('/extract', (req, res) => {
   if (!req.files && !req.files.pdfFile) {
